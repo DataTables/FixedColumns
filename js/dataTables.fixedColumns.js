be0c17c2320fdd55a3b0ef6d84a57c677e5e279f
John Bezanis <bezanis@temple.edu>
2019-10-24 17:42:15 -0400

    lock column scrolling
    Fix scrolling when moving from left columns to main columns on a single scroll, and vice versa

--- js/dataTables.fixedColumns.js
+++ js/dataTables.fixedColumns.js
@@ -542,7 +542,7 @@ $.extend( FixedColumns.prototype , {
 					mouseController = 'main';
 				}
 
-				if ( mouseController === 'main' ) {
+				if ( mouseController === 'main' || mouseController === 'left') {
 					if ( that.s.iLeftColumns > 0 ) {
 						that.dom.grid.left.liner.scrollTop = that.dom.scroller.scrollTop;
 					}
@@ -569,7 +569,7 @@ $.extend( FixedColumns.prototype , {
 						mouseController = 'left';
 					}
 
-					if ( mouseController === 'left' ) {
+					if ( mouseController === 'left' || mouseController === 'main' ) {
 						that.dom.scroller.scrollTop = that.dom.grid.left.liner.scrollTop;
 						if ( that.s.iRightColumns > 0 ) {
 							that.dom.grid.right.liner.scrollTop = that.dom.grid.left.liner.scrollTop;
