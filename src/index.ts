7bc6dcd5053b2d6128798325bbdf202dc14ef38c
Allan Jardine <allan@datatables.net>
2022-11-07 16:09:09 +0000

    Release 4.2.1 (packaging error with 4.2.0)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-/*! FixedColumns 4.2.0
+/*! FixedColumns 4.2.1
  * 2019-2022 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     FixedColumns
  * @description FixedColumns extension for DataTables
- * @version     4.2.0
+ * @version     4.2.1
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @copyright   SpryMedia Ltd.
  *
