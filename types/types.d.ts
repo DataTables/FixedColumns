639b1fab3287f1136c7b908a49fbe2de38640172
Allan Jardine <allan@datatables.net>
2023-07-03 10:45:42 +0100

    Fix: Correct type inheritance for styling packages

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,98 +1,95 @@
-// Type definitions for DataTables FixedColumns
-//
-// Project: https://datatables.net/extensions/fixedcolumns/, https://datatables.net
+// Type definitions for datatables.net-fixedcolumns 3.2
+// Project: https://datatables.net
+// Definitions by: Konstantin Kuznetsov <https://github.com/Arik-neKrol>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.4
 
 /// <reference types="jquery" />
+/// <reference types="datatables.net"/>
 
-import DataTables, {Api} from 'datatables.net';
-import {IDefaults} from './FixedColumns';
+declare namespace DataTables {
+    interface Settings {
+        /*
+         * FixedColumns extension options
+         */
+        fixedColumns?: boolean | FixedColumnsSettings;
+    }
 
-export default DataTables;
+    interface FixedColumnsSettings {
+        /*
+         * Row height matching algorithm to use
+         *
+         * The algorithm to use. This can be one of (see below for full description):
+         * 'none' | 'semiauto' | 'auto'
+         */
+        heightMatch?: 'none' | 'semiauto' | 'auto';
 
-type DeepPartial<T> = T extends object ? {
-    [P in keyof T]?: DeepPartial<T[P]>;
-} : T;
+        /*
+         * The number of columns on the left hand side of the table to fix in place.
+         */
+        leftColumns?: number;
 
+        /*
+         * The number of columns on the right hand side of the table to fix in place.
+         */
+        rightColumns?: number;
+    }
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * DataTables' types integration
- */
-declare module 'datatables.net' {
-	interface Config {
-		/**
-		 * FixedColumns extension options
-		 */
-		fixedColumns?: boolean | ConfigFixedColumns;
-	}
+    interface Api {
+        /**
+        * Namespacing for FixedColumns methods - FixedColumns' methods are available on the returned API instance.
+        * 
+        * @returns DataTables API instance with the FixedColumns methods available.
+        */
+        fixedColumns(): FixedColumnsMethods | Api;
+    }
 
-	interface ConfigLanguage {
-		/**
-		 * StateRestore language options
-		 */
-		 fixedColumns?: ConfigFixedColumnsLanguage;
-	}
+    interface FixedColumnsMethods extends Api {
+        /**
+        * @Deprecated(use dt.cell(this).index())
+        * Get the cell index of a cell in a fixed column
+        * 
+        * @param row The cell (td or th) to get the cell index of. This can be either a cell in the fixed columns or in the host DataTable.
+        * @returns The cell index for the given cell.
+        */
+        cellIndex(row: JQuery | Node): CellIndex;
+        
+        /**
+         * Redraw the fixed columns based on new table size
+         * 
+         * @returns DataTables API instance
+         */
+        relayout(): Api;
+        
+        /**
+         * @Deprecated(use dt.row(this).index())
+         * Get the row index of a row in a fixed column
+         * 
+         * @param row The row (tr) to get the row index of. This can be either a row in the fixed columns or in the host DataTable.
+         * @returns The row index for the given row
+         */
+        rowIndex(row: JQuery | Node): number;
 
-	interface Api<T> {
-		/**
-		 * FixedColumns API Methods
-		 */
-		fixedColumns(): ApiFixedColumns<T>;
-	}
+        /**
+        * Update the data shown in the FixedColumns
+        * 
+        * @returns DataTables API instance
+        */
+        update(): Api;
+    }
 
-	interface ApiStatic {
-		/**
-		 * FixedColumns class
-		 */
-		FixedColumns: {
-			/**
-			 * Create a new FixedColumns instance for the target DataTable
-			 */
-			new (dt: Api<any>, settings: string[] | ConfigFixedColumns | ConfigFixedColumns[]);
+    /*
+    */
+    interface CellIndex {
+        row: number;
+        column: number;
+        columnVisible: number;
+    }
 
-			/**
-			 * FixedColumns version
-			 */
-			version: string;
-
-			/**
-			 * Default configuration values
-			 */
-			defaults: ConfigFixedColumns;
-		}
-	}
-}
-
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Options
- */
-interface ConfigFixedColumns extends DeepPartial<IDefaults> {}
-
-interface ConfigFixedColumnsLanguage extends DeepPartial<IDefaults['i18n']> {}
-
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * API
- */
-
-interface ApiFixedColumns<T> extends Api<T> {
-	/**
-	 * Get the number of fixed left columns
-	 */
-	left(): number;
-
-	/**
-	 * Set the number of fixed left columns
-	 */
-	left(val: number): Api<T>;
-
-	/**
-	 * Get the number of fixed right columns
-	 */
-	right(): number;
-
-	/**
-	 * Set the number of fixed right columns
-	 */
-	right(val: number): Api<T>;
+    interface RowsMethods {
+        /*
+         * Recalculate the height of one or more rows after a data change
+         */
+        recalcHeight(): Api;
+    }
 }
