a8415e733e2b4cf56cd628a753cfd15deb6bfa86
Allan Jardine <allan@datatables.net>
2023-10-26 14:34:03 +0100

    Dev: Foundation types file tweak

--- types/fixedColumns.foundation.d.ts
+++ types/fixedColumns.foundation.d.ts
@@ -3,6 +3,3 @@ import DataTable from 'datatables.net-fixedcolumns';
 
 export default DataTable;
 export * from 'datatables.net-fixedcolumns';
-/*! Foundation integration for DataTables' FixedColumns
- * © SpryMedia Ltd - datatables.net/license
- */
