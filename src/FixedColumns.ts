f5b5ec8066104d02302d5f759b82044edfc080b7
Allan Jardine <allan@datatables.net>
2026-04-06 15:26:19 +0000

    Release 6.0.0-beta.1

--- src/FixedColumns.ts
+++ src/FixedColumns.ts
@@ -42,7 +42,7 @@ export interface IDOM {
 }
 
 export default class FixedColumns {
-	private static version = '6.0.0-dev';
+	private static version = '6.0.0-beta.1';
 
 	private static classes: IClasses = {
 		bottomBlocker: 'dtfc-bottom-blocker',
